d5fa2f9b8c18e551b4384006402c3e5099ea6d0a
Igor Perazzoli <igorperazzoli06@gmail.com>
2021-12-12 14:13:07 -0300

    Adicionando js e pequenas mudanças

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function changeToLogin() {
+    window.location.href = "login_screen.html";
+}
+
+function changeToRegister() {
+    window.location.href = "register_screen.html";
+}
+
+function changeToIndex() {
+    window.location.href = "index.html";
+}
