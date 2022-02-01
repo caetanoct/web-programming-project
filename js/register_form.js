1ecb166a0f4f3be2553e9b710e3d24e716b16eeb
caetanoct <caetanocolintorres@gmail.com>
2022-02-01 15:44:59 -0300

    cpf mascara

--- js/register_form.js
+++ js/register_form.js
@@ -1,8 +1,52 @@
+const cpf = document.getElementById("cpf");
+cpf.setAttribute("maxlength","14");
 const password = document.getElementById("pass1");
 const confpassword = document.getElementById("pass2");
 const button = document.querySelector("button");
 // button.onclick(printCheckedRadio());
 button.addEventListener("click",registerFormButtonPressed);
+cpf.addEventListener("input", maskCPF);
+cpf.addEventListener("keypress", highlightOnNaN);
+function highlightOnNaN () {
+    if (isNaN(cpf.value[cpf.value.length-1])){        
+        cpf.style.background = "red";
+        cpf.value = prev_value;
+        return;
+    } else {
+        cpf.style.background = "white";
+    }
+}
+var cpfHelpTextAdded = false;
+function maskCPF () {
+    console.log(cpf.value);
+    let prev_value = ""
+    if (cpf.value.length > 1) {
+        prev_value = cpf.value.substring(0,cpf.value.length-1);
+    }
+    if (isNaN(cpf.value[cpf.value.length-1])){        
+        cpf.style.background = "Salmon";
+        if (!cpfHelpTextAdded) {
+            var tag = document.createElement("small");
+            var text = document.createTextNode("Por favor, Digite números para o CPF.")
+            tag.appendChild(text);
+            var element = document.querySelector(".form-row");
+            element.appendChild(tag);
+            cpfHelpTextAdded = true;
+        }
+        cpf.value = prev_value;
+        return;
+    } else {
+        cpf.style.background = "white";
+        if (cpfHelpTextAdded) {
+            var element = document.querySelector(".form-row");
+            element.removeChild(element.lastChild);
+            cpfHelpTextAdded = false;
+        }
+    }
+    if (cpf.value.length == 3) cpf.value += ".";
+    if (cpf.value.length == 7) cpf.value += ".";
+    if (cpf.value.length == 11) cpf.value += "-";
+}
 function registerFormButtonPressed () {
     validatePassword();
 }
--- js/services_form.js
+++ js/services_form.js
@@ -5,44 +5,55 @@ const pet_name = document.getElementById("pet_name");
 const markup_date_picker = document.getElementById("markup_date_picker");
 const radio_group = document.getElementsByName('radio_group');
 // adding listeners
-// button.onclick(printCheckedRadio());
-service_type_select.addEventListener("change",printSelectedOption);
+// button.onclick(getCheckedRadio());
+service_type_select.addEventListener("change",getSelectedOption);
 button.addEventListener("click",printAppointment);
-pet_name.addEventListener("blur",printPetName);
-markup_date_picker.addEventListener("blur",printDate);
+pet_name.addEventListener("blur",getPetName);
+markup_date_picker.addEventListener("blur",getDate);
 // add listener for all radio group items
 for (i = 0; i < radio_group.length; i++) {
-    radio_group[i].addEventListener("change",printCheckedRadio);
+    radio_group[i].addEventListener("change",getCheckedRadio);
 }
 // FUNCTIONS
-function printDate () {
-    console.log(markup_date_picker.value)
+function getDate () {
+    return markup_date_picker.value;
 }
 function printAppointment() {
-    printPetName();
-    printDate();
-    printSelectedOption();
-    printCheckedRadio();
+    let name = getPetName();
+    let date = getDate();
+    let option = getSelectedOption();
+    let pet_size = getCheckedRadio();
+    console.log("name="+name+" date="+date+" option="+option+" petsize="+pet_size);
 }
-function printPetName () {
+function getPetName () {
     let name = pet_name.value;
-    console.log(name);
+    return name;
+    //console.log(name);
 }
-function printCheckedRadio () {
+function getCheckedRadio () {
     let collection = radio_group;
     for(i = 0; i < collection.length; i++) {
-        if(collection[i].checked) {     
-            console.log(collection[i].value + " -- " + collection[i].id);
+        if(collection[i].checked) {
+            switch (collection[i].value) {
+                case "option1":
+                    return "Grande";
+                case "option2":
+                    return "Médio";
+                case "option3":
+                    return "Pequeno";
+            }
+            // console.log(collection[i].value + " -- " + collection[i].id);             
         }
     }
 }
 
-function printSelectedOption() {
+function getSelectedOption() {
     let element = service_type_select;
     if (element.value == 0) {
-        console.log("Por favor, selecione o tipo de serviço.");
+        window.alert("Por favor, selecione o tipo de serviço.");        
     } else {
         let selectedStr = element.options[element.selectedIndex].text;
-        console.log(`A opção selecionada tem o indice [${element.value}] e o valor de "${selectedStr}"`);
+        return selectedStr;
+        //console.log(`A opção selecionada tem o indice [${element.value}] e o valor de "${selectedStr}"`);
     }    
 }
