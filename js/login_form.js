5b875e39473cba30ac387bff3ce0fd813fba37b8
caetanoct <caetanocolintorres@gmail.com>
2022-02-02 20:36:26 -0300

    arrumado borda para dispositivos mobile + email

--- js/login_form.js
+++ js/login_form.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+const email_element = document.getElementById("mail");
+email_element.addEventListener("blur",mail_blur_handler);
+function mail_blur_handler() {
+    validate_email(email_element.value) == true ? email_element.style.background = "White" : email_element.style.background = "Salmon";    
+}
+// receveis email as a string, and return true if the email passes the regex test.
+function validate_email(email) {
+    // match negated sets of not whitespace and not @ character with one or more plus a @ symbol and negated set . negated set.
+    let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
